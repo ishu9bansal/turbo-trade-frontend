4e94439ffb5f2abe601883f910c314841b2041de
Ishu Bansal <ishubansal1400@gmail.com>
2025-06-04 18:36:09 +0530

    include a page to show the available contracts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BacktestForm from "./components/BacktestForm";
 import Layout from "./components/Layout";
+import ContractScatterPlot from "./components/ContractScatterPlot"; // Adjust path if needed
 
 export default function App() {
   return (
-    <Layout>
-      <BacktestForm />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<BacktestForm />} />
+          <Route path="/plot" element={<ContractScatterPlot />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
