f34487a18713a6a77e1d4dba6f4892705adfc3c5
tewarishash12 <tewarishashwat2002@gmail.com>
2025-06-13 13:48:55 +0530

    created a single allRoutes that contain all the routes which are present in the code

--- src/routes/allRoutes.tsx
+++ src/routes/allRoutes.tsx
@@ -1,17 +1,26 @@
 import BacktestForm from "../components/BacktestForm";
 import ContractScatterPlot from "../components/ContractScatterPlot";
+import LandingPage from "../pages/LandingPage";
 
-const protectedRoutes = [
+const allRoutes = [
+  {
+    path: "/",
+    element: <LandingPage />,
+    label: "Home",
+    public: true
+  },
   {
     path: "/backtest",
     element: <BacktestForm />,
     label: "Backtest",
+    public: false
   },
   {
     path: "/plot",
     element: <ContractScatterPlot />,
     label: "Plot",
+    public: false
   },
 ];
 
-export default protectedRoutes;
+export default allRoutes;
