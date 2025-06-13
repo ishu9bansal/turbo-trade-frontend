e2229081549c7964b8aefe60fa6f140d44bbb6f1
tewarishash12 <tewarishashwat2002@gmail.com>
2025-06-13 10:47:16 +0530

    Updated the address of layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./components/Layout/Layout";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 import protectedRoutes from "./routes/protectedRoutes";
