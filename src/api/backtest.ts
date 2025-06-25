83a84bba69006f49d25bb44365786af591d2dfb3
tewarishash12 <tewarishashwat2002@gmail.com>
2025-06-25 13:58:03 +0530

    Updated backtest calls based on the changes made in the backend

--- src/api/backtest.ts
+++ src/api/backtest.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { type BacktestFormData } from "../types/orchestrator";
 import type { Contract, RawOrder } from "../types/types";
+import { DEFAULT_FORM_DATA } from "..//types/constants";
 
 // Define the shape of the API response
 export interface BacktestResponse {
@@ -11,36 +12,38 @@ export interface BacktestResponse {
 
 interface FullResponse {
   message: string;
-  tradeResult: BacktestResponse;
+  backtestId: BacktestResponse;
 }
 
-const BASE_URL = import.meta.env.VITE_BACKEND_PYTHON_URL;
-// const BASE_URL_2 = import.meta.env.VITE_BACKEND_NODE_URL;
-const BASE_URL_3 = import.meta.env.VITE_API_BASE_URL
+const BASE_URL_2 = import.meta.env.VITE_BACKEND_NODE_URL;
 
 
 export async function postBacktest(config: BacktestFormData, token: string): Promise<BacktestResponse> {
   try {
-    const url_1 = `${BASE_URL_3}/backtests?response=success`;
-    // const url_2 = `${BASE_URL_3}/api/strategy/add-strategy`;
-    const response = await axios.post<FullResponse>(url_1, config, {
+    const url_2 = `${BASE_URL_2}/backtests/create`;
+    const response = await axios.post<FullResponse>(url_2, config, {
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json"
       }
     });
     console.log(response.data);
-    return response.data.tradeResult;
+    return response.data.backtestId;
   } catch (error: any) {
     console.error("Error during backtest:", error);
     throw new Error(error?.response?.data?.detail || "Unknown error");
   }
 }
 
-export async function getContracts(): Promise<Contract[]> {
+export async function getContracts(token:string): Promise<Contract[]> {
   try {
-    const url = `${BASE_URL}/contracts/NIFTY`;
-    const response = await axios.get<Contract[]>(url);
+    const url = `${BASE_URL_2}/python/contracts`;
+    const response = await axios.get<Contract[]>(url, 
+      {
+        headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
     return response.data;
   } catch (error: any) {
     console.error("Error fetching contracts:", error);
@@ -48,39 +51,46 @@ export async function getContracts(): Promise<Contract[]> {
   }
 }
 
-export const getStrategyHistory = async () => {
+export async function getConfig(token:string): Promise<BacktestFormData> {
   try {
-    const response = await fetch(`${BASE_URL_3}/backtests`, {
-      method: "GET",
+    const url = `${BASE_URL_2}/python/config`;
+    const response = await axios.get<BacktestFormData>(url,{
       headers: {
-        "Content-Type": "application/json",
-      },
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error("Error fetching config:", error);
+    // throw new Error(error?.response?.data?.detail || "Unknown error");
+    return DEFAULT_FORM_DATA;
+  }
+}
+
+export async function getBacktestHistory(token: string) {
+  try {
+    const url = `${BASE_URL_2}/backtests/user`;
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+    const backtests = response.data.backtests; // comes from backend response
 
-    const data = await response.json();
-
-    // Transform data into simplified strategy cards
-    const formatted = data.history.map((item) => {
-      console.log(item);
-      const firstOrder = item.results?.orders?.[0] || {};
-      console.log(firstOrder)
-      return {
-        id: item.id,
-        date: new Date(item.created_at).toISOString().split("T")[0],
-        symbol: firstOrder.contract?.symbol || "N/A",
-        type: item?.config?.strategy_name || "Strategy", // fallback if not present
-        capital: item?.config?.capital || 0,
-        status: item.status || "Unknown",
-      };
-    });
+    const formatted = backtests.map((item: any) => ({
+      id: item._id,
+      date: new Date(item.created_at).toISOString().split("T")[0],
+      symbol: item.strategy?.position?.focus?.symbol || "N/A",
+      type: "Backtest",
+      capital: item.strategy?.capital || 0,
+      status: item.status || "Unknown"
+    }));
 
     return formatted;
-  } catch (error) {
+
+  } catch (error: any) {
     console.error("Failed to fetch strategy history:", error.message);
     return [];
   }
-};
+}
